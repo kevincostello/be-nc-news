92b5f598b7ca60ce362713a27e5c48629f1efc4f
Kevin Costello <kevin_m_costello@hotmail.com>
2020-01-13 15:48:09 +0000

    Solved formatDates for an array with one object

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,20 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  if (list.length === 0) return [];
+  else {
+    // console.log("list:", list[0]);
+    const listNew = [];
+    const listDate = new Date(list[0].created_at); // parse values into a javascript Date object
+
+    const newObj = { created_at: listDate };
+    delete list[0].created_at;
+    listNew.push(newObj); // push ISO date onto new array
+    // listNew.push(...list[0], newObj.created_at);
+    const spreadNew = [...list];
+    spreadNew[0].created_at = newObj.created_at;
+    console.log("This is spreadNew", spreadNew);
+    return spreadNew;
+  }
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,48 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("when given an empty array return a new empty array", () => {
+    expect(formatDates([])).to.deep.equal([]);
+  });
+  it("when given an array of one object with only a date field, return a new array with one object with the date converted to a Javascript date object", () => {
+    const onlyDate = [{ created_at: 1468087638932 }];
+    const convertDate = new Date(onlyDate[0].created_at);
+    const expectedArray = [{ created_at: convertDate }];
+    expect(formatDates(onlyDate)).to.deep.equal(expectedArray);
+  });
 
-describe('makeRefObj', () => {});
+  it("when given an array of one element with all fields, return a new array with the element and all fields plus the date converted field", () => {
+    const articleObject = [
+      {
+        article_id: 1,
+        title: "Peanuts",
+        topic: "Comedy",
+        body: "This is a comic strip about a boy and his dog and friends",
+        votes: 34,
+        author: "Charles Schultz",
+        created_at: 1509866563519
+      }
+    ];
+    const outputObject = [
+      {
+        article_id: 1,
+        title: "Peanuts",
+        topic: "Comedy",
+        body: "This is a comic strip about a boy and his dog and friends",
+        votes: 34,
+        author: "Charles Schultz"
+      }
+    ];
+    outputObject[0].created_at = new Date(articleObject[0].created_at);
+    expect(formatDates(articleObject)).to.deep.equal(outputObject);
+  });
+});
 
-describe('formatComments', () => {});
+describe("makeRefObj", () => {});
+
+describe("formatComments", () => {});
