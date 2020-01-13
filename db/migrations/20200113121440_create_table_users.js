4cd03d9686e342e740e77cf41f5d9073b005a525
Kevin Costello <kevin_m_costello@hotmail.com>
2020-01-13 12:21:22 +0000

    Migrated users table

---
+++ db/migrations/20200113121440_create_table_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  console.log("Creating users table", process.env.NODE_ENV);
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .primary()
+      .unique();
+    usersTable.string("avatar_url");
+    usersTable.string("name");
+  });
+};
+
+exports.down = function(knex) {
+  console.log("Dropping users table", process.env.NODE_ENV);
+  return knex.schema.dropTable("users");
+};
