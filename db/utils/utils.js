6f13682e842fb12928951bfd8a42c564b859ff20
Kevin Costello <kevin_m_costello@hotmail.com>
2020-01-13 16:17:55 +0000

    Solved changing date on object to Javascript date object and return all the other properties unchanged

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,18 +1,13 @@
 exports.formatDates = list => {
   if (list.length === 0) return [];
   else {
-    // console.log("list:", list[0]);
-    const listNew = [];
-    const listDate = new Date(list[0].created_at); // parse values into a javascript Date object
-
-    const newObj = { created_at: listDate };
-    delete list[0].created_at;
-    listNew.push(newObj); // push ISO date onto new array
-    // listNew.push(...list[0], newObj.created_at);
-    const spreadNew = [...list];
-    spreadNew[0].created_at = newObj.created_at;
-    console.log("This is spreadNew", spreadNew);
-    return spreadNew;
+    const listOut = list.map(article => {
+      // loop through each article in the array
+      const listDate = new Date(article.created_at); // format unix date to be a Javascript date object
+      article.created_at = listDate; // replace created_at property with new Javascript date object
+      return article; // return the updated object
+    });
+    return listOut; // return the updated array
   }
 };
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -9,14 +9,14 @@ describe("formatDates", () => {
   it("when given an empty array return a new empty array", () => {
     expect(formatDates([])).to.deep.equal([]);
   });
-  it("when given an array of one object with only a date field, return a new array with one object with the date converted to a Javascript date object", () => {
+  it("when given an array of one object with only a date property, return a new array with one object with the date converted to a Javascript date object", () => {
     const onlyDate = [{ created_at: 1468087638932 }];
     const convertDate = new Date(onlyDate[0].created_at);
     const expectedArray = [{ created_at: convertDate }];
     expect(formatDates(onlyDate)).to.deep.equal(expectedArray);
   });
 
-  it("when given an array of one element with all fields, return a new array with the element and all fields plus the date converted field", () => {
+  it("when given an array of one object with all poperties, return a new array with the object and all properties plus the date converted field", () => {
     const articleObject = [
       {
         article_id: 1,
@@ -40,6 +40,63 @@ describe("formatDates", () => {
     ];
     outputObject[0].created_at = new Date(articleObject[0].created_at);
     expect(formatDates(articleObject)).to.deep.equal(outputObject);
+  });
+
+  it("when given an array of multiple objects, return a new array with multiple objects and the modified Javascript date property", () => {
+    const articleArrayOfObjects = [
+      {
+        article_id: 1,
+        title: "Peanuts",
+        topic: "Comedy",
+        body: "This is a comic strip about a boy and his dog and friends",
+        votes: 34,
+        author: "Charles Schultz",
+        created_at: 1509866563519
+      },
+      {
+        article_id: 2,
+        title: "Beano",
+        topic: "Comedy",
+        body: "This is a comic about various characters who get up to no good",
+        votes: 16,
+        author: "DC Thomson",
+        created_at: 1509866563512
+      }
+    ];
+
+    const articleOutputArrayOfObjects = [
+      {
+        article_id: 1,
+        title: "Peanuts",
+        topic: "Comedy",
+        body: "This is a comic strip about a boy and his dog and friends",
+        votes: 34,
+        author: "Charles Schultz"
+      },
+      {
+        article_id: 2,
+        title: "Beano",
+        topic: "Comedy",
+        body: "This is a comic about various characters who get up to no good",
+        votes: 16,
+        author: "DC Thomson"
+      }
+    ];
+    articleOutputArrayOfObjects[0].created_at = new Date(
+      articleArrayOfObjects[0].created_at
+    );
+    articleOutputArrayOfObjects[1].created_at = new Date(
+      articleArrayOfObjects[1].created_at
+    );
+    expect(formatDates(articleArrayOfObjects)).to.deep.equal(
+      articleOutputArrayOfObjects
+    );
+  });
+
+  it("returns an unmutated array when passed an array of multiple objects", () => {
+    expect(formatDates(articleArrayOfObjects)).to.deep.equal(
+      articleOutputArrayOfObjects
+    );
   });
 });
 
