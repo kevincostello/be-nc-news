df166128fd0e16c5e4792ce46ed45a8a14b5be44
Kevin Costello <kevin_m_costello@hotmail.com>
2020-01-17 08:17:46 +0000

    Solved GET request with status of 200 to return array of comments by article_id sorted by created_at ordered in descending order

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -6,6 +6,7 @@ const chaiSorted = require("chai-sorted");
 const { expect } = chai;
 const db = require("../db/connection");
 
+chai.use(require("sams-chai-sorted"));
 // console.log("db:", db);
 
 describe("/api", () => {
@@ -177,6 +178,28 @@ describe("/api", () => {
             "author",
             "created_at"
           ]);
+        });
+    });
+
+    it.only("GETS a status code of 200 and returns an array of sorted comments by the created_at for a given article id when valid queries are passed in the request with created_at as the sort by column and desc as the order by value", () => {
+      return request(app)
+        .get("/api/articles/1/comments?sort_by=author")
+        .expect(200)
+        .then(dbResponse => {
+          expect(dbResponse.body).to.be.an("array");
+          expect(dbResponse.body[0]).to.be.an("object");
+          expect(dbResponse.body.length).to.equal(13);
+          expect(dbResponse.body[0]).to.have.keys([
+            "article_id",
+            "comment_id",
+            "body",
+            "votes",
+            "author",
+            "created_at"
+          ]);
+          expect(dbResponse.body).to.be.sortedBy("created_at", {
+            descending: true
+          });
         });
     });
   });
