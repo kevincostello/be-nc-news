ca0820d22740e839e43d09671fc1a7dc51a2b14e
Kevin Costello <kevin_m_costello@hotmail.com>
2020-01-20 08:58:05 +0000

    Moved GET (/api) to api-router.js

--- routes/api-router.js
+++ routes/api-router.js
@@ -3,7 +3,7 @@ const topicsRouter = require("./topics-router");
 const usersRouter = require("./users-router.js");
 const articlesRouter = require("./articles-router");
 const commentsRouter = require("./comments-router");
-
+const endpoints = require("../endpoints.json");
 const { allAPIendpoints } = require("../controllers/api-controller");
 
 const { send405Error } = require("../errors/index");
@@ -12,9 +12,16 @@ apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
+// apiRouter
+//   .route("/")
+//   .get(allAPIendpoints)
+//   .all(send405Error);
+
 apiRouter
-  .route("/")
-  .get(allAPIendpoints)
+  // .route("/")
+  .get("/", function(req, res, next) {
+    res.status(200).send({ endpoints });
+  })
   .all(send405Error);
 
 module.exports = apiRouter;
